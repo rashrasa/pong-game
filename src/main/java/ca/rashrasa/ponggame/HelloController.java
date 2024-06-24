576726bc8850c3e4a27bb42edc4a2d836ef7524d
rashrasa <ahmed.rashrash@gmail.com>
2024-06-24 18:57:53 -0400

    initial commit

---
+++ src/main/java/ca/rashrasa/ponggame/HelloController.java
@@ -0,0 +1,14 @@
+package ca.rashrasa.ponggame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
